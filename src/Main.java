21f742ac84f84a97ceb4595460033737e65ce18e
Tatiana Kosarevskaya <tv.kosarevskaya@gmail.com>
2021-05-19 12:54:16 +0300

    Main changing

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ public class Main {
         int balance = 2_000_000_000;
         int transfer = 500_000_000;
         int total = balance + transfer;
-        System.out.println(total);
+        System.out.println("New balance:" + total);
     }
 }
